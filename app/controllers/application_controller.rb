79e5e3104b60e81f487ecc27dbf7ce40b807ebad
siu-sing <lai.siusing@gmail.com>
2020-08-15 18:39:24 +0800

    modified user new and edit, added more links to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :gender, :height, :weight])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :gender, :height, :weight])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :workouts
+  resources :workouts do
+    resources :records
+  end
   resources :records
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
